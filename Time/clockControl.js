d68826fcc4d89ca00e146080d0152e2d399c6e44
lezinst <robstlezin@gmail.com>
2016-05-25 22:44:52 +0100

    added minute and second hand code

--- Time/clockControl.js
+++ Time/clockControl.js
@@ -1,12 +1,49 @@
-angular.module('timeController').controller('clockControl', timeControl);
+angular.module('clock').controller('clockControl', function(){
+  var clockControl = this;
+  
+  clockControl.secondHand = document.getElementById('seconds');
+  clockControl.minuteHand = document.getElementById('minutes');
+  var handTracker = 6;
 
-$scope.secondHand = document.getElementsByClassName('hr');
+  // var style = window.getComputedStyle(clockControl.secondHand, null).getPropertyValue("transform");
+  
+  clockControl.startMinuteHand = function(){
+			clockControl.minuteHand.style.transform = "rotate(" +handTracker+ "deg)";
+			handTracker+=6;
+	}
 
-function startClock(){
-    document.getElementsByClassName('hr').onload = function(){
-        if(this.innerHTML === 'Play'){
-            this.innerHTML === 'Pause';
-            
-        }
-    }
-}
+  clockControl.startHands = function(){ 
+		var x = 0;
+			setInterval(function(){
+				if(clockControl.secondHand.style.transform === "rotate(0deg)") {
+					clockControl.startMinuteHand();
+				}
+				if(clockControl.secondHand.style.transform === "rotate(354deg)") {
+					x = 0;
+					console.log('reset 0 degrees', clockControl.secondHand.style.transform);
+
+				}
+				
+				clockControl.secondHand.style.transform = "rotate(" +x+ "deg)";
+				x+=6;
+				console.log(clockControl.secondHand.style.transform);
+			},1000);
+
+
+	}
+
+   clockControl.startClock = function(){
+		if(clockControl.secondHand.style.transform = "none") {
+			clockControl.startHands();
+		}
+	}
+
+	
+
+		
+		
+	
+	
+	 
+});
+
--- Time/timeControl.js
+++
@@ -1,5 +0,0 @@
-angular.module('clock').controller('clockControl', timeControl);
-
-function startClock(){
-    this.time = 
-}
